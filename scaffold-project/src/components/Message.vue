22c9c8394f4893c43056f00061ed09b9b7320c6c
Marvin Senkiv <127746129+marvinsenkiv@users.noreply.github.com>
2023-06-16 15:31:07 -0300

    added if else

--- scaffold-project/src/components/Message.vue
+++ scaffold-project/src/components/Message.vue
@@ -1,17 +1,10 @@
 
 
 <template>
-    <section class="grid-area3">            
-        <div class="msg-box">                                       
-            <p class="msg-in" >Lorem ipsum dolor sit amet, consectetur</p>                                                                  
-            <p class="msg-out">adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-            <p class="msg-out">sit amet, consectetur, adipisci</p>
-            <p class="msg-in" >But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects,</p>  
-            <p class="msg-out">dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequence</p>
-            <p class="msg-in" >which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with </p>  
-            <p class="msg-in" >usto odio dignissimos ducimus qui blanditiis praese</p>  
-            <p class="msg-out">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil imped</p>
-            <p class="msg-in" >Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperior</p>  
+    <section class="grid-area3">   
+        <div class="msg-box" v-for="item in messagesFromApi"> 
+            <p v-if="item.type === 'in'"  class="msg-in" >{{item.message}}</p> 
+            <p v-if="item.type === 'out'"  class="msg-out" >{{item.message}}</p>                                                                 
         </div>             
       </section>
 </template>
@@ -20,7 +13,20 @@
   export default {
     data() {
       return {
-        dynamicContents: ['Conteúdo dinâmico 1', 'Conteúdo dinâmico 2']
+        messagesFromApi: [
+        {
+          phone: "119999999",
+          message: "Mensagem oi",
+          type: "out",
+          date: "16/03/2023 14:00"
+        },
+        {
+          phone: "119999999",
+          message: "Mensagem oi",
+          type: "out",
+          date: "16/03/2023 14:00"
+        }
+      ]
       };
     }
   };
@@ -45,6 +51,7 @@
 }
 
 .msg-in {
+    margin-left: 0.3%;
     display: flex;
     margin-right: auto;
     padding: 4px 10px 4px 0px;
@@ -54,6 +61,7 @@
 }
 
 .msg-out {
+    margin-right: 0.3%;
     display: flex;
     justify-content: right;
     padding: 4px 0px 4px 10px;
